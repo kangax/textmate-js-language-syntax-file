d1ba68f31b0c7832eb83cf6af77f9b37bcc11d1b
kangax <kangax@gmail.com>
2010-10-05 22:48:30 -0400

    Add test file. Update readme. Fix numeric literals (with exponent) matching. Add ES5 array methods.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ### Enhancements
 
-- Built-ins from ES5 (e.g. `JSON`)
+- Built-ins from ES5 (e.g. `JSON`) added to the list of global objects
+- ES5 array methods added to the list of built-in methods
+- Numeric literals with exponents (e.g. 123e-12) are highlighted properly
---
+++ test.js
@@ -0,0 +1,45 @@
+/* These array methods should be highlighted */
+
+[].concat()
+[].join()
+[].pop()
+[].push()
+[].reverse()
+[].shift()
+[].slice()
+[].indexOf()
+[].lastIndexOf()
+[].every()
+[].some()
+[].forEach()
+[].map()
+[].filter()
+[].reduce()
+[].reduceRight()
+
+/* JSON should be highlighted */
+
+JSON.stringify()
+
+/* These should all be highlighted as literals */
+
+1.23e12
+-435.12
+-9e99
+8e-4567
+12E-13
+13E14
+1e+2
+23.12e-2347
+
+/* These console methods should be all highlighted identically */
+
+console.warn
+console.info
+console.log
+console.error
+console.time
+console.timeEnd
+console.assert
+console.profile
+console.debug
